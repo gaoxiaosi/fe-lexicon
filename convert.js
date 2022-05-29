4c5a04c5b226e4619deb2d5a7a195caaa0971e8e
wupeng <836720919@qq.com>
2022-05-29 22:14:58 +0800

    新增重复词汇判断

--- convert.js
+++ convert.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 
+// TODO: 处理markdown的正则，待优化
 const reg = /##[^\n]*\n(.*?)(?=\n##?\s|$)/gm
 
 /** 
@@ -9,9 +10,13 @@ const reg = /##[^\n]*\n(.*?)(?=\n##?\s|$)/gm
 const convert = () => {
   let data = fs.readFileSync('./README.md', 'utf-8');
   let found = data.match(reg);
-  let arr = found.map(item => item.split('\n')[1].replace(/\s/g, '\n') + '\n');
-  let result = arr.reduce((pre, cur) =>  pre + cur)
-  fs.writeFileSync('sougou.text', result, 'utf-8')
+  let arr = found.reduce((pre, cur) => pre + cur.split('\n')[1] + ' ', '').split(' ');
+  let repeats = arr.filter((item, index) => arr.indexOf(item) !== index);
+  if (repeats.length === 0) {
+    fs.writeFileSync('sougou.text', arr.join('\n'), 'utf-8');
+  } else {
+    throw new Error(`有重复项：${repeats}`);
+  }
 }
 
 module.exports = convert
