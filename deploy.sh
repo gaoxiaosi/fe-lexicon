7a554bab0698163b353d97def6eedcd5d4f79ec8
wupeng <836720919@qq.com>
2022-11-20 23:07:43 +0800

    优化部署文档

--- deploy.sh
+++ deploy.sh
@@ -9,10 +9,10 @@ pnpm run build
 # 进入构建文件夹
 cd dist
 
-# git init
-# git checkout -b main
-# git add -A
-# git commit -m 'deploy'
+git init
+git checkout -b main
+git add -A
+git commit -m 'deploy'
 
 # 部署在 https://<USERNAME>.github.io/<REPO>
 git push -f git@github.com:gaoxiaosi/fe-lexicon.git main:gh-pages
