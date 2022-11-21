0b8a26219cf737b06a0f116d0fae9d79b211ef65
wupeng <836720919@qq.com>
2022-11-21 15:44:01 +0800

    优化部署脚本

--- deploy.sh
+++ deploy.sh
@@ -9,10 +9,13 @@ pnpm run build
 # 进入构建文件夹
 cd dist
 
-git init
-git checkout -b main
+time=$(date "+%Y-%m-%d %H:%M")
+
+# 初次部署的时候需要这两条命令
+# git init
+# git checkout -b main
 git add -A
-git commit -m 'deploy'
+git commit -m "deploy at ${time}"
 
 # 部署在 https://<USERNAME>.github.io/<REPO>
 git push -f git@github.com:gaoxiaosi/fe-lexicon.git main:gh-pages
